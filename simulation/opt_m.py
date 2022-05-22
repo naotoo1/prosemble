d9ce0250f2479140a5b7c9b09a2476faf0b48f9b
Nana Abeka Otoo <82911284+naotoo1@users.noreply.github.com>
2022-05-22 18:30:15 +0200

    Update opt_m.py

--- simulation/opt_m.py
+++ simulation/opt_m.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from hybrid12 import Hybrid
+from prosemble import Hybrid
 import matplotlib.pyplot as plt
 import numpy as np
 

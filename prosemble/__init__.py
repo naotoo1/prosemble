ff84b1d38e1a8371e70145874468f09a5143e3a2
Nana Abeka Otoo <abekaotoo@gmail.com>
2023-09-24 03:25:11 +0200

    added version

--- prosemble/__init__.py
+++ prosemble/__init__.py
@@ -4,5 +4,7 @@ Prosemble package
 from . import models
 from . import core
 
-__version__ = "0.7.0"
-__author__ = 'Nana Abeka Otoo'
+
+__author__ = """Nana Abeka Otoo"""
+__email__ = 'nanaabekaotoo@gmail.com'
+__version__ = '0.7.0'

c40a645c1bd7d1901dc4b024a313aaacd5bed2eb
Nana Abeka Otoo <82911284+naotoo1@users.noreply.github.com>
2023-09-24 13:57:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],

f59ce16f298991fcc8c37f21cc90ac18c720eef0
nana abeka otoo <abekaotoo@gmail.com>
2025-03-01 00:51:48 +0100

    WIP :  setup in devenv

---
+++ devenv.nix
@@ -0,0 +1,45 @@
+# one with the docker container
+{ pkgs, lib, config, inputs, ... }:
+
+let
+  pythonPackages = pkgs.python312Packages;
+  project_dir = "${config.env.DEVENV_ROOT}";
+  env.PYTHONPATH = "${pkgs.python312Packages.sitePackages}";
+in {
+  cachix.enable = false;
+  env.PROJECT_DIR = project_dir;
+
+  packages = with pkgs; [
+    git
+    git-lfs
+    nixpkgs-fmt
+    pythonPackages.numpy
+    pythonPackages.matplotlib
+    pythonPackages.scikit-learn
+    pythonPackages.pandas
+    pythonPackages.scipy
+    pythonPackages.pip
+
+  ];
+
+  # Add Docker support
+  # containers.docker = {
+  #   isSystemd = true;
+  #   ephemeral = true;
+  #   privateUsers = true;
+  #   extraOptions = ["--network=host"];
+  # };
+
+  languages = {
+    python = {
+      enable = true;
+      package = pythonPackages.python;
+      uv.enable = true;
+      venv = {
+        enable = true;
+      };
+    };
+  };
+
+  starship.enable = true;
+}

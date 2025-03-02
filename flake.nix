f55e25c029f2e0137d2a7ff704c8a37e67cf115e
nana abeka otoo <abekaotoo@gmail.com>
2025-03-02 17:48:21 +0100

    flakes update

--- flake.nix
+++ flake.nix
@@ -7,9 +7,11 @@
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, flake-utils, devenv, ... }:
+  outputs = { self, nixpkgs, flake-utils, devenv, ... }@inputs:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: {
-      devShells.default = devenv.lib.mkShell {
+      devShells.default = let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in devenv.lib.mkShell {
         inherit inputs pkgs;
         modules = [
           ./devenv.nix

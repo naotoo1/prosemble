1705dce85702175fd0840cdb4d9129b4e6b91564
nana abeka otoo <abekaotoo@gmail.com>
2025-03-02 18:18:52 +0100

    udated workflows

--- devenv.nix
+++ devenv.nix
@@ -1,13 +1,61 @@
-# one with the docker container
-{ pkgs, lib, config, inputs, ... }:
+# # one with the docker container
+# { pkgs, lib, config, inputs, ... }:
+
+# let
+#   pythonPackages = pkgs.python312Packages;
+#   project_dir = "${config.env.DEVENV_ROOT}";
+#   env.PYTHONPATH = "${pkgs.python312Packages.sitePackages}";
+# in {
+#   cachix.enable = false;
+#   env.PROJECT_DIR = project_dir;
+
+#   packages = with pkgs; [
+#     git
+#     git-lfs
+#     nixpkgs-fmt
+#     pythonPackages.numpy
+#     pythonPackages.matplotlib
+#     pythonPackages.scikit-learn
+#     pythonPackages.pandas
+#     pythonPackages.scipy
+#     pythonPackages.pip
+#     docker
+#     docker-compose
+
+#   ];
+
+#   # Add Docker support
+#   # containers.docker = {
+#   #   isSystemd = true;
+#   #   ephemeral = true;
+#   #   privateUsers = true;
+#   #   extraOptions = ["--network=host"];
+#   # };
+
+#   languages = {
+#     python = {
+#       enable = true;
+#       package = pythonPackages.python;
+#       uv.enable = true;
+#       venv = {
+#         enable = true;
+#       };
+#     };
+#   };
+
+#   starship.enable = true;
+# }
+
+# devenv.nix
+{ pkgs, lib, config, ... }:
 
 let
   pythonPackages = pkgs.python312Packages;
   project_dir = "${config.env.DEVENV_ROOT}";
-  env.PYTHONPATH = "${pkgs.python312Packages.sitePackages}";
 in {
   cachix.enable = false;
   env.PROJECT_DIR = project_dir;
+  env.PYTHONPATH = "${pythonPackages.python.sitePackages}";
 
   packages = with pkgs; [
     git
@@ -21,7 +69,6 @@ in {
     pythonPackages.pip
     docker
     docker-compose
-
   ];
 
   # Add Docker support
--- flake.nix
+++ flake.nix
@@ -1,38 +1,19 @@
 {
   description = "Development environment for Prosemble project";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
   };
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, devenv, ... }@inputs:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: {
       devShells.default = let
         pkgs = nixpkgs.legacyPackages.${system};
-        pythonPackages = pkgs.python312Packages;
-      in pkgs.mkShell {
-        buildInputs = with pkgs; [
-          bashInteractive
-          git
-          git-lfs
-          nixpkgs-fmt
-          python312
-          pythonPackages.numpy
-          pythonPackages.matplotlib
-          pythonPackages.scikit-learn
-          pythonPackages.pandas
-          pythonPackages.scipy
-          pythonPackages.pip
-          pythonPackages.uv
-          docker
-          docker-compose
+      in devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          ./devenv.nix
         ];
-        shellHook = ''
-          # Set up environment variables
-          export PROJECT_DIR=$PWD
-          export PYTHONPATH="${pythonPackages.sitePackages}:$PYTHONPATH"
-        '';
       };
     });
 }

7025ce31891b373a80ca9e1c48f4137ef9c8af33
nana abeka otoo <abekaotoo@gmail.com>
2025-03-02 23:21:39 +0100

    updated the packages

--- devenv.nix
+++ devenv.nix
@@ -1,61 +1,13 @@
-# # one with the docker container
-# { pkgs, lib, config, inputs, ... }:
-
-# let
-#   pythonPackages = pkgs.python312Packages;
-#   project_dir = "${config.env.DEVENV_ROOT}";
-#   env.PYTHONPATH = "${pkgs.python312Packages.sitePackages}";
-# in {
-#   cachix.enable = false;
-#   env.PROJECT_DIR = project_dir;
-
-#   packages = with pkgs; [
-#     git
-#     git-lfs
-#     nixpkgs-fmt
-#     pythonPackages.numpy
-#     pythonPackages.matplotlib
-#     pythonPackages.scikit-learn
-#     pythonPackages.pandas
-#     pythonPackages.scipy
-#     pythonPackages.pip
-#     docker
-#     docker-compose
-
-#   ];
-
-#   # Add Docker support
-#   # containers.docker = {
-#   #   isSystemd = true;
-#   #   ephemeral = true;
-#   #   privateUsers = true;
-#   #   extraOptions = ["--network=host"];
-#   # };
-
-#   languages = {
-#     python = {
-#       enable = true;
-#       package = pythonPackages.python;
-#       uv.enable = true;
-#       venv = {
-#         enable = true;
-#       };
-#     };
-#   };
-
-#   starship.enable = true;
-# }
-
-# devenv.nix
-{ pkgs, lib, config, ... }:
+# one with the docker container
+{ pkgs, lib, config, inputs, ... }:
 
 let
   pythonPackages = pkgs.python312Packages;
   project_dir = "${config.env.DEVENV_ROOT}";
+  env.PYTHONPATH = "${pkgs.python312Packages.sitePackages}";
 in {
   cachix.enable = false;
   env.PROJECT_DIR = project_dir;
-  env.PYTHONPATH = "${pythonPackages.python.sitePackages}";
 
   packages = with pkgs; [
     git
@@ -67,8 +19,9 @@ in {
     pythonPackages.pandas
     pythonPackages.scipy
     pythonPackages.pip
-    # docker
-    # docker-compose
+    docker
+    docker-compose
+
   ];
 
   # Add Docker support
@@ -92,3 +45,4 @@ in {
 
   starship.enable = true;
 }
+

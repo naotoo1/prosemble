09572f7245fc8f1aa45b33cc147578c519d664e5
Nana Abeka Otoo <nana.abekaotoo@authentic.network>
2026-05-23 23:15:26 +0200

    chore: Bump version to 2.2.0

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'Prosemble'
 copyright = '2024-2026, Nana Abeka Otoo'
 author = 'Nana Abeka Otoo'
-release = '2.1.2'
+release = '2.2.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
